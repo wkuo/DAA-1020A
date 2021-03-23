897edebb0faa2c7fa1e73d45cae16b779a68efde
wkuo <wuichia@gmail.com>
2021-03-23 13:09:57 +0800

    Call the correct program

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,request,Response
 from botbuilder.core import BotFrameworkAdapter,BotFrameworkAdapterSettings,TurnContext,ConversationState,MemoryStorage
 from botbuilder.schema import Activity
 import asyncio
-from hero_card import SampleAnimationCard
+from pic_button import SampleAnimationCard
 
 
 app = Flask(__name__)
